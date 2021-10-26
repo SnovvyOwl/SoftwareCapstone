a23686bf20997386dbf29a442baec78c355c34c8
SnovvyOwl <owl0614@naver.com>
2021-10-26 21:37:17 +0900

    update

---
+++ FasterRCNN/WaymoDataset.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import torch
+from PIL import Image
+import pickle
+from multiprocessing import Queue, Process, Pool
+
+WAYMO_CLASSES = ['unknown', 'Vehicle', 'Pedestrian', 'Sign', 'Cyclist']
+
+class WaymoDataset(torch.utils.data.Dataset):
+    def __init__(self,root,segment):
+        self.root=root
+        self.segment=segment
+        self.img_path=self.root+segment+"/img/"
+        self.imgs=list(sorted(os.listdir(self.img_path)))[3:]
+        self.ann_path=self.img_path+"camera_"+segment+'.pkl'
+        self.front_imgs=self.imgs[:199]
+        self.front_left_imgs=self.imgs[199:398]
+        self.front_right_imgs=self.imgs[398:597]
+        self.side_left_imgs=self.imgs[597:796]
+        self.side_right_imgs=self.imgs[796:]
+        self.front_anno=[]
+        self.front_left_anno=[]
+        self.front_right_anno=[]
+        self.side_left_anno=[]
+        self.side_right_anno=[]
+        self.anno=self.get_annotation()
+    
+    def get_annotation(self):
+        anno=[]
+        with open(self.ann_path, 'rb') as f:
+            groundtruth=pickle.load(f)
+        for i in range(len(groundtruth)):
+            if i%5==0:
+                self.front_anno.append(groundtruth[i])
+            elif i%5==1:
+                self.front_left_anno.append(groundtruth[i])
+            elif i%5==2:
+                self.front_right_anno.append(groundtruth[i])
+            elif i%5==3:
+                self.side_left_anno.append(groundtruth[i])
+            elif i%5==4:
+                self.side_right_anno.append(groundtruth[i])   
+        
+        return self.front_anno+self.front_left_anno+self.front_right_anno+self.side_left_anno+self.side_right_anno
+    
+    def __getitem__(self,idx):
+        img_path = os.path.join(self.img_path, self.imgs[idx])
+        img=Image.open(img_path).convert("RGB")
+        target=self.anno[idx]
+        
+        return img, target
+        
+if __name__=="__main__":
+    root="./PV_R-CNN/data/waymo/waymo_processed_data/"
+    sequece='segment-1024360143612057520_3580_000_3600_000_with_camera_labels'
+    test=WaymoDataset(root,sequece)
+    test.__getitem__(800)
