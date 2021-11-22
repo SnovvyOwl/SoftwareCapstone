6f541edf024a43a90e820ca85b434baa35338f58
SnovvyOwl <owl0614@naver.com>
2021-11-22 21:17:30 +0900

    Et

--- pratice.py
+++ pratice.py
@@ -8,6 +8,7 @@ import open3d as o3d
 from tensorflow.python.eager.context import PhysicalDevice
 from queue import Queue
 import matplotlib.pyplot as plt
+import torch
 class viewbox(object):
     def __init__(self,minx,miny,maxx,maxy):
         self.min=np.array([[minx],[miny],[1]])
@@ -43,10 +44,13 @@ def projection(intrinsic,extrinsic,lidar,weight,height):
                         idx.append(i)
     return in_box_point,idx
 
-def segmentation(frustrum,label,box,idx):
+def segmentation(frustrum,idx):
     cluster=[]
-    centroid_vector=[(frustrum[:,0].max()+frustrum[:,0].min())/2,(frustrum[:,1].max()+frustrum[:,1].min())/2,(frustrum[:,2].max()+frustrum[:,2].min())/2]
-    centroid_vector=[frustrum[:,0].min(),frustrum[:,1].mean()/frustrum[:,0].mean()*frustrum[:,0].min(),(frustrum[:,2]).mean()/frustrum[:,0].mean()*frustrum[:,0].min()]
+    # centroid_vector=[(frustrum[:,0].max()+frustrum[:,0].min())/2,(frustrum[:,1].max()+frustrum[:,1].min())/2,(frustrum[:,2].max()+frustrum[:,2].min())/2]
+    radius=(frustrum[:,0]**2+frustrum[:,1]**2+frustrum[:,2]**2)**0.5
+    radius=radius[np.argmin(radius)]
+    mean_radius=(frustrum[:,0].mean()**2+frustrum[:,1].mean()**2+frustrum[:,2].mean()**2)**0.5
+    centroid_vector=[frustrum[:,0].mean()/mean_radius*radius,frustrum[:,1].mean()/mean_radius*radius,(frustrum[:,2]).mean()/mean_radius*radius]
     vec=o3d.geometry.LineSet()
     vecpoint=[[0,0,0],centroid_vector]
     line=[[0,1]]
@@ -60,6 +64,8 @@ def segmentation(frustrum,label,box,idx):
             points.append(point)
             idices.append(idx[i])
     cluster=make_cluster(centroid,fi,frustrum,idx,0.01)   
+    # cluster.append(idx[605])
+    # cluster=idx
     # cluster=make_cluster(frustrum)   
     return cluster,vec
 
@@ -81,13 +87,84 @@ def make_cluster(centroid,centroid_idx,frustrum,idx,max_radius=0.1):
             if radius<max_radius:
                 cluster.append(cp_idx[i])
                 points.put(point)
-                print(idx[i])
                 next_cp_idx=np.delete(cp_idx,i)
                 next_cp_frustrum=np.delete(cp_frustrum,i,0)
         cp_idx=next_cp_idx
         cp_frustrum=next_cp_frustrum
     return cluster
+
+def make_3dBox(anno):
+    boxes=[]
+    lines=[[0,1],[1,2],[2,3],[0,3],[0,4],[4,5],[5,6],[6,7],[4,7],[1,5],[2,6],[3,7]]
+    corners3d=boxes_to_corners_3d(anno["boxes_lidar"])
+    for box in corners3d:
+        box3d=o3d.geometry.LineSet()
+        box3d.points= o3d.utility.Vector3dVector(box)
+        box3d.lines=o3d.utility.Vector2iVector(lines)
+        boxes.append(box3d)
+    return boxes
     
+
+
+
+def check_numpy_to_torch(x):
+    if isinstance(x, np.ndarray):
+        return torch.from_numpy(x).float(), True
+    return x, False
+
+
+def rotate_points_along_z(points, angle):
+    """
+    Args:
+        points: (B, N, 3 + C)
+        angle: (B), angle along z-axis, angle increases x ==> y
+    Returns:
+
+    """
+    points, is_numpy = check_numpy_to_torch(points)
+    angle, _ = check_numpy_to_torch(angle)
+
+    cosa = torch.cos(angle)
+    sina = torch.sin(angle)
+    zeros = angle.new_zeros(points.shape[0])
+    ones = angle.new_ones(points.shape[0])
+    rot_matrix = torch.stack((
+        cosa,  sina, zeros,
+        -sina, cosa, zeros,
+        zeros, zeros, ones
+    ), dim=1).view(-1, 3, 3).float()
+    points_rot = torch.matmul(points[:, :, 0:3], rot_matrix)
+    points_rot = torch.cat((points_rot, points[:, :, 3:]), dim=-1)
+    return points_rot.numpy() if is_numpy else points_rot
+
+
+def boxes_to_corners_3d(boxes3d):
+    """
+        7 -------- 4
+       /|         /|
+      6 -------- 5 .
+      | |        | |
+      . 3 -------- 0
+      |/         |/
+      2 -------- 1
+    Args:
+        boxes3d:  (N, 7) [x, y, z, dx, dy, dz, heading], (x, y, z) is the box center
+
+    Returns:
+    """
+    boxes3d, is_numpy = check_numpy_to_torch(boxes3d)
+
+    template = boxes3d.new_tensor((
+        [1, 1, -1], [1, -1, -1], [-1, -1, -1], [-1, 1, -1],
+        [1, 1, 1], [1, -1, 1], [-1, -1, 1], [-1, 1, 1],
+    )) / 2
+
+    corners3d = boxes3d[:, None, 3:6].repeat(1, 8, 1) * template[None, :, :]
+    corners3d = rotate_points_along_z(corners3d.view(-1, 8, 3), boxes3d[:, 6]).view(-1, 8, 3)
+    corners3d += boxes3d[:, None, 0:3]
+
+    return corners3d.numpy() if is_numpy else corners3d
+
 def find_centroid(center_vector,frustrum,idx):
     centroid=None
     radius=(frustrum[:,1]-center_vector[1])**2+(frustrum[:,2]-center_vector[2])**2
@@ -107,15 +184,14 @@ if __name__ == "__main__":
 
     xyz=np.load("/home/seongwon/SoftwareCapstone/data/waymo/waymo_processed_data/segment-1024360143612057520_3580_000_3600_000_with_camera_labels/0000.npy")
     xyz=xyz[:,:3]
-
   
     pcd = o3d.geometry.PointCloud()
     label =frustrum[0]["frustrum"][0]
     frustrum=xyz[label["frustrum"]]
-    seg ,vec=segmentation(frustrum,label,annos2d[0]["anno"][0]["boxes"][0],label["frustrum"])
-  
-    pcd.points = o3d.utility.Vector3dVector(xyz[seg])
+    seg ,vec=segmentation(frustrum,label["frustrum"])
 
+    pcd.points = o3d.utility.Vector3dVector( xyz[seg])
+    box=make_3dBox(annos3d[0])
     # cameranum=4
     # # cameranum=int(input("input cameranum"))
     # if cameranum in [0,1,2]:
@@ -134,4 +210,17 @@ if __name__ == "__main__":
     all=o3d.geometry.PointCloud()
     all.points=o3d.utility.Vector3dVector(cp_xyz)
     all.paint_uniform_color([0,0,1])
-    o3d.visualization.draw_geometries([pcd,all,vec])
+    point=[pcd,all]
+    box.append(pcd)
+    box.append(all)
+    vis=o3d.visualization.Visualizer()
+    vis.create_window()
+    for b in box:
+        vis.add_geometry(b)
+    vis.add_geometry(all)
+    vis.add_geometry(pcd)
+    vis.get_render_option().line_width = 100
+    vis.update_renderer()
+
+    vis.run()
+    vis.destroy_window()
