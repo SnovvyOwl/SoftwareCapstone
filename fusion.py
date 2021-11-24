ac49786089ea6d69285549a3fe822b7754613b1a
SnovvyOwl <owl0614@naver.com>
2021-11-24 22:53:43 +0900

    et'

--- fusion.py
+++ fusion.py
@@ -4,11 +4,78 @@ from modelmanager import ModelManager
 import numpy as np
 import torch
 import pickle
-import torch.distributed as dist
-import torch.multiprocessing as mp
-import os
+
 CAMMERA_NUM = 5
 
+def make_3dBox(anno):
+    boxes=[]
+    lines=[[0,1],[1,2],[2,3],[0,3],[0,4],[4,5],[5,6],[6,7],[4,7],[1,5],[2,6],[3,7]]
+    corners3d=boxes_to_corners_3d(anno["boxes_lidar"])
+    for box in corners3d:
+        box3d=o3d.geometry.LineSet()
+        box3d.points= o3d.utility.Vector3dVector(box)
+        box3d.lines=o3d.utility.Vector2iVector(lines)
+        boxes.append(box3d)
+    return boxes
+    
+
+def check_numpy_to_torch(x):
+    if isinstance(x, np.ndarray):
+        return torch.from_numpy(x).float(), True
+    return x, False
+
+
+def rotate_points_along_z(points, angle):
+    """
+    Args:
+        points: (B, N, 3 + C)
+        angle: (B), angle along z-axis, angle increases x ==> y
+    Returns:
+
+    """
+    points, is_numpy = check_numpy_to_torch(points)
+    angle, _ = check_numpy_to_torch(angle)
+
+    cosa = torch.cos(angle)
+    sina = torch.sin(angle)
+    zeros = angle.new_zeros(points.shape[0])
+    ones = angle.new_ones(points.shape[0])
+    rot_matrix = torch.stack((
+        cosa,  sina, zeros,
+        -sina, cosa, zeros,
+        zeros, zeros, ones
+    ), dim=1).view(-1, 3, 3).float()
+    points_rot = torch.matmul(points[:, :, 0:3], rot_matrix)
+    points_rot = torch.cat((points_rot, points[:, :, 3:]), dim=-1)
+    return points_rot.numpy() if is_numpy else points_rot
+
+
+def boxes_to_corners_3d(boxes3d):
+    """
+        7 -------- 4
+       /|         /|
+      6 -------- 5 .
+      | |        | |
+      . 3 -------- 0
+      |/         |/
+      2 -------- 1
+    Args:
+        boxes3d:  (N, 7) [x, y, z, dx, dy, dz, heading], (x, y, z) is the box center
+
+    Returns:
+    """
+    boxes3d, is_numpy = check_numpy_to_torch(boxes3d)
+
+    template = boxes3d.new_tensor((
+        [1, 1, -1], [1, -1, -1], [-1, -1, -1], [-1, 1, -1],
+        [1, 1, 1], [1, -1, 1], [-1, -1, 1], [-1, 1, 1],
+    )) / 2
+
+    corners3d = boxes3d[:, None, 3:6].repeat(1, 8, 1) * template[None, :, :]
+    corners3d = rotate_points_along_z(corners3d.view(-1, 8, 3), boxes3d[:, 6]).view(-1, 8, 3)
+    corners3d += boxes3d[:, None, 0:3]
+
+    return corners3d.numpy() if is_numpy else corners3d
 
 class Fusion(object):
     def __init__(self, root, ckpt):
@@ -69,8 +136,6 @@ class Fusion(object):
         with open("frustrum.pkl", 'wb') as f:
             pickle.dump(result, f)
         return result
-
-
 
     def pointcloud2image(self, lidar):
         point_planes = []
