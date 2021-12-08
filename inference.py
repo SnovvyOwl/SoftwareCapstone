fa9b8bcde416271957c8cfe6332ce31ec5d5c6fe
SnovvyOwl <owl0614@naver.com>
2021-12-08 21:57:43 +0900

    et

--- inference.py
+++ inference.py
@@ -1,26 +1,33 @@
+import enum
 import pickle
 import numpy as np
 import torch
-import open3d as o3d
 from fusion import Fusion
 from PVRCNN.ops.iou3d_nms.iou3d_nms_utils import boxes_iou3d_gpu
-class MeanAveragePrecision(object):
-    def __init__(self):
-        self.true_positive=[]
-        self.score=[]
+
+class AveragePrecision(object):
+    def __init__(self,_name):
+        self.true_positive=np.array([])
         self.gt_len=0
         self.detection_len=0
-        self.name=None
+        self.name=_name
     
-    def setting(self,name,gt_len,detection_len):
-        self.name=name
-        self.gt_len=gt_len
+    def frame_add(self,gt_len,detection_len):
+        self.gt_len+=gt_len
         self.detection_len=detection_len
+    def add(self,score,tp,iou):
+        if self.true_positive.shape[0]!=0:
+            self.true_positive=np.vstack((self.true_positive,np.array([score,tp,iou])))
+        else:
+            self.true_positive=np.array([score,tp,iou])
+       
+    def get_AP(self):
+        coffindence=[0.9,0.8,0.7,0.6,0.5,0.4,0.3,0.2,0.1,0.0]
+        for cs in coffindence:
+            current=np.where(self.true_positive[:,0]>cs)
+            print(cs)
+        return NotImplementedError
     
-    def add(self,score,tp):
-        self.true_positive.append(tp)
-        self.score.append(score)
-
 class Inference(object):
     def __init__(self,root,ckptdir):
         self.root=root
@@ -31,18 +38,20 @@ class Inference(object):
         self.PVRCNN_result=None
         self.result_of_fusion=None
         self.updated_result=[]
+        self.pedestrianAP=AveragePrecision("Pedestrian")
+        self.vehicleAP=AveragePrecision("Vehicle")
+        self.cyclistAP=AveragePrecision("Cyclist")
         # self.fusion=Fusion(root,ckptdir)
     
     def load_ground_truth_data(self):
         dirpath="./data/waymo/waymo_infos_val.pkl"
         with open(dirpath,"rb") as f:
             self.gt_data=pickle.load(f)
+    @staticmethod
+    def find_max(gt_idx,fram_idx,iou_mat):
+        return gt_idx[np.argmax(iou_mat[fram_idx,gt_idx])]
 
     def main(self):
-        all_gt=0
-        all_detection=0
-        all_TP=0
-        all_FP=0
         self.load_ground_truth_data()
         # self.result_of_fusion,self.PVRCNN_result= self.fusion.main()
         # self.PVRCNN_result=annos3d
@@ -54,37 +63,67 @@ class Inference(object):
         for frame in self.updated_result:
             for gt_frame in self.gt_data:
                 if frame["frame_id"]==gt_frame["frame_id"]:
+                    # TP
                     # Calcluate IoU For One Frame
                     sign_idx=np.where(gt_frame["annos"]["name"]=="Sign")
                     iou_mat=boxes_iou3d_gpu(torch.tensor(gt_frame["annos"]["gt_boxes_lidar"].astype("float32")).cuda(),torch.tensor(frame["boxes_lidar"]).cuda())
                     iou_mat=iou_mat.cpu().numpy()
                     iou_mat=np.delete(iou_mat,sign_idx,axis=0)
                     gt_name=np.delete(gt_frame["annos"]["name"],sign_idx,axis=0)
-                    frame_all_gt,frame_all_detection,frame_TP,frame_FP=self.match_correction(gt_name,frame["name"],iou_mat,frame["score"],0.3)
-                    all_gt+=frame_all_gt
-                    all_detection+=frame_all_detection
-                    all_TP+=frame_TP
-                    all_FP+=frame_FP
+                    self.match_correction(gt_name,frame["name"],iou_mat,frame["score"],0.5)
+                
                     break
-        print("True Prediction: {0}".format(all_TP))
-        print("ALL_GT: {0}".format(all_gt))
-        print("ALL_Det: {0}".format(all_detection))
-        print("Precision : {0}".format(float(all_TP)/float(all_detection)))
-        print("Recall : {0}".format(float(all_TP)/float(all_gt)))
-                    
-    def match_correction(self,gt_name,frame_name,iou_mat,score,thresh=0.4):
-        TP=0
-        FP=0
-        match_result=np.array(np.where(iou_mat>thresh)).T
-        for match in match_result:
-            if gt_name[match[0]]==frame_name[match[1]]:
-                TP+=1
-            else:
-                FP+=1
-        all_detection=iou_mat.shape[1]
-        all_gt=len(gt_name)
-        return all_gt,all_detection,TP,FP
-    
+        self.pedestrianAP.get_AP()
+        # print("True Prediction: {0}".format(all_TP))
+        # print("ALL_GT: {0}".format(all_gt))
+        # print("ALL_Det: {0}".format(all_detection))
+        # print("Precision : {0}".format(float(all_TP)/float(all_detection)))
+        # print("Recall : {0}".format(float(all_TP)/float(all_gt)))
+  
+
+    def match_correction(self,gt_name,frame_name,_iou_mat,score,thresh=0.5):
+        iou_mat=_iou_mat.T
+        for i, name in enumerate(frame_name):
+            if name =="Pedestrian":
+                gt_idx=np.where(iou_mat[i]>0)[0]
+                if len(gt_idx)==0:
+                    self.pedestrianAP.add(score[i],0,0)
+                else:
+                    if len(gt_idx)>1:
+                        gt_idx=self.find_max(gt_idx,i,iou_mat)
+                    if gt_name[gt_idx]==frame_name[i]:
+                        found=(iou_mat[i,gt_idx]>thresh)
+                    else:
+                        found=0
+                    self.pedestrianAP.add(score[i],found,iou_mat[i,gt_idx])
+            elif name =="Vehicle":
+                gt_idx=np.where(iou_mat[i]>0)[0]
+                if len(gt_idx)==0:
+                    self.vehicleAP.add(score[i],0,0)
+                else:
+                    if len(gt_idx)>1:
+                        gt_idx=self.find_max(gt_idx,i,iou_mat)
+                    if gt_name[gt_idx]==frame_name[i]:
+                        found=(iou_mat[i,gt_idx]>thresh)
+                    else:
+                        found=0
+                    self.vehicleAP.add(score[i],found,iou_mat[i,gt_idx])
+            elif name =="Cyclist":
+                gt_idx=np.where(iou_mat[i]>0)[0]
+                if len(gt_idx)==0:
+                    self.cyclistAP.add(score[i],0,0)
+                else:
+                    if len(gt_idx)>1:
+                        gt_idx=self.find_max(gt_idx,i,iou_mat)
+                    if gt_name[gt_idx]==frame_name[i]:
+                        found=(iou_mat[i,gt_idx]>thresh)
+                    else:
+                        found=0
+                    self.cyclistAP.add(score[i],found,iou_mat[i,gt_idx])  
+        self.pedestrianAP.frame_add(len(np.where(gt_name=="Pedestrian")[0]),len(np.where(frame_name=="Pedestrian")[0]))
+        self.vehicleAP.frame_add(len(np.where(gt_name=="Vehicle")[0]),len(np.where(frame_name=="Vehicle")[0]))
+        self.cyclistAP.frame_add(len(np.where(gt_name=="Cyclist")[0]),len(np.where(frame_name=="Cyclist")[0]))
+
     def add_result(self): 
         for result_frame in self.result_of_fusion:
             update_frame={}
